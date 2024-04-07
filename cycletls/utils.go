461a594e3b3d3efd1920e7e4a0fe129b3557a964
galerts <93411916+galerts@users.noreply.github.com>
2024-04-08 02:40:45 +0500

    Update utils.go

--- cycletls/utils.go
+++ cycletls/utils.go
@@ -352,7 +352,8 @@ func genMap() (extMap map[string]utls.TLSExtension) {
 		"65281": &utls.RenegotiationInfoExtension{
 			Renegotiation: utls.RenegotiateOnceAsClient,
 		},
-		"65037": utls.BoringGREASEECH(),
+		//"65037": utls.BoringGREASEECH(),
+		"65037": &utls.GREASEEncryptedClientHelloExtension{},
 	}
 	return
 
