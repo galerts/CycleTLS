484febeed098c1ead4326707f78876806bc9dd2e
galerts <93411916+galerts@users.noreply.github.com>
2024-06-07 17:56:13 +0500

    Update connect.go

--- cycletls/connect.go
+++ cycletls/connect.go
@@ -14,8 +14,8 @@ import (
 	"strconv"
 	"sync"
 
-	http "github.com/Danny-Dasilva/fhttp"
-	http2 "github.com/Danny-Dasilva/fhttp/http2"
+	http "github.com/galerts/fhttp"
+	http2 "github.com/galerts/fhttp/http2"
 	"golang.org/x/net/proxy"
 	"h12.io/socks"
 )
